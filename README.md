aa7594873be65c5cfbdfe9bc5b609cc88395d987
Sergio Ruiz Aragón <sergio.ruiz@delonia.com>
2017-07-26 10:44:01 +0200

    Entry field and text to speech to probe the functionality

---
+++ README.md
@@ -0,0 +1,11 @@
+## Synopsis
+
+I want to create a WhatsApp Speaker by myself. I know there are some of them in Google Play or Apple Store.
+
+## Installation
+
+You need to download Visual Studio and Xamarin for Visual Studio before build and deploy this project.
+
+## API Reference
+
+I'm using Xamarin Forms and Xamarin.Plugins.TextToSpeech NuGet package
---
+++ WhatsAppSpeaker/HomePage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Plugin.TextToSpeech;
+using Xamarin.Forms;
+
+namespace WhatsAppSpeaker
+{
+    public class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+			Entry entry1 = new Entry
+			{
+				Placeholder = "Introduce el texto a decir"
+			};
+
+			Button btn1 = new Button
+			{
+				Text = "Empezar a escuchar",
+				Command = new Command(() =>
+				{
+                    var text = entry1.Text;
+
+					CrossTextToSpeech.Current.Speak(text);
+				})
+			};
+
+			StackLayout stk = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Children = { entry1, btn1 }
+			};
+
+			Content = stk;
+        }
+    }
+}
