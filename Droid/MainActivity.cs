7700c8ed1fb796e12035dfa879b6aa9c43473f5b
Sergio Ruiz Aragón <sergio.ruiz@delonia.com>
2017-07-26 12:50:50 +0200

    New Interfaces and Native Droid CS Component added. Assembly dependencies done.

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -11,6 +11,7 @@ using Android.OS;
 namespace WhatsAppSpeaker.Droid
 {
     [Activity(Label = "WhatsAppSpeaker.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [IntentFilter(new[] { "android.service.notification.NotificationListenerService" })]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
---
+++ Droid/NotificationsReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Android;
+using WhatsAppSpeaker.Droid;
+using Android.Service.Notification;
+
+[assembly: Dependency(typeof(NotificationsReader))]
+namespace WhatsAppSpeaker.Droid
+{
+    public class NotificationsReader : INotificationsReader
+    {
+        public NotificationsReader()
+        {
+        }
+
+        public string NotificationsText()
+        {
+            NotificationListenerService nls = new NotificationListenerService();
+            /*if (nls != null)
+            {
+                foreach (StatusBarNotification notification in nls.GetActiveNotifications())
+                {
+
+                }
+            }*/
+
+
+        }
+    }
+}
